060ea49fb9a16fda3c68da68b7795a8de3bbdd01
Varun Kumar <varun.kumar@yale.edu>
2023-10-23 12:43:54 -0400

    email testing

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,9 +1,9 @@
 class OrderMailer < ApplicationMailer
-  default from: 'varun.kumar@hey.com'
+  default from: email_address_with_name('varkum@outlook.com', 'Kumars Kombucha')
 
   def subscription_email
     @email = params[:email]
-    mail(to: @email.address, subject: "You've subscribed to Kumars Kombucha")
+    mail(to: @email.address, subject: "You've Subscribed to Kumars Kombucha")
   end
 
   def order_email

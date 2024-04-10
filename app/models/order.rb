8bb5c410b8aba8d448c5f314643a3ae70ae22976
Varun Kumar <varun.kumar@yale.edu>
2024-04-09 21:04:46 -0400

    add pickup days

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,7 @@ class Order < ApplicationRecord
   belongs_to :batch
   
   enum :status, [ :placed, :completed, :reminded, :incomplete ], default: :placed
-  enum :pickup_day, [ :tuesday, :wednesday, :thursday, :friday ]
+  enum :pickup_day, [ :monday, :tuesday, :wednesday, :thursday, :friday, :saturday, :sunday ]
   
   scope :unfulfilled, -> { where(status: "placed") }
 end
